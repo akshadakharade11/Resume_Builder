b9e17d98be25b195176e01cceb1dc8d174257c79
Akshada Kharade <64721714+akshadakharade11@users.noreply.github.com>
2025-10-20 21:50:39 +0530

    Refactor UserController to AuthController with endpoints

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -5,13 +5,31 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.example.demo.dtos.AuthResponse;
+import com.example.demo.dtos.UserLoginRequest;
 import com.example.demo.dtos.UserRegisterRequest;
 import com.example.demo.dtos.UserResponse;
 import com.example.demo.entity.User;
+import com.example.demo.services.AuthService;
 import jakarta.validation.Valid;
 
 
-public class UserController {
-	
-	
+@RestController
+@RequestMapping("api/auth")
+public class AuthController {
+
+	@Autowired
+	private AuthService authService;
+
+	@PostMapping("/register")
+	public ResponseEntity<AuthResponse> register(@RequestBody UserRegisterRequest request) {
+		return ResponseEntity.ok(authService.register(request));
+	}
+
+	@PostMapping("/login")
+	public ResponseEntity<AuthResponse> login(@RequestBody UserLoginRequest request) {
+		return ResponseEntity.ok(authService.login(request));
+	}
 }
